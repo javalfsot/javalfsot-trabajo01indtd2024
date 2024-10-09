043973a04eef408320a40441694ad785271d98b2
javalfsot <2003javias2013@gmail.com>
2024-10-09 17:15:27 +0200

    Enunciados resoluciones.R
    Creación de los enunciados de Problema I y Problema II del archivo de resolución.

---
+++ resoluciones.R
@@ -0,0 +1,55 @@
+# Teoría de la Decisión || Trabajo 01 Individual || Parte 2, Resolución --------
+
+
+
+## Problema I ------------------------------------------------------------------
+
+
+### Enunciado I ----------------------------------------------------------------
+
+# Teniendo la matriz:       e1  e2  e3  e4  e5 
+#                      d1    9  11   6   3  12
+#                      d2    4   2   8   7  13
+#                      d3   10   1   4   8   7
+# 
+# Resuelve el problema con cada uno de los métodos o funciones individuales de
+# Incertidumbre por separado (tanto en situación favorable como desfavorable).
+
+
+### Solución I -----------------------------------------------------------------
+
+
+
+## Problema II -----------------------------------------------------------------
+
+
+### Enunciado II ---------------------------------------------------------------
+
+# Supongamos que un inversor deportivo debe decidir entre tres posibles
+# estrategias a la hora de destinar su dinero en la temporada 24/25 de Dallas
+# Mavericks, el equipo de la liga estadounidense de baloncesto, la NBA.
+#
+# Estas estrategias de inversión son marketing (estrategia A), infraestructura (B)
+# o apostar por jugadores jóvenes (C).
+#
+# Pueden generar diferentes beneficios dependiendo del rendimiento del equipo
+# texano. Las cuatro posibles situaciones y los beneficios según los escenarios
+# son las siguientes:
+#
+# - Si el equipo no clasifica a play-off: la estrategia A da 25 millones de
+# dólares; la B da 15 y la C, 20.
+#
+# - Si Dallas clasifica a play-off pero no a las finales, con las estrategias se 
+# obtiene un beneficio de 35, 45 y 40 millones ($) respectivamente.
+# 
+# - Si pierden la final los beneficios serán de 50, 55 y 45 (en millones de $).
+#
+# - Por último, si Dallas Mavericks resulta ganador del anillo, los beneficios se
+# irían a 70 millones con la estrategia A, 65 millones con la B y 80 con la C.
+#
+#
+# Resuelve este problema mediante todos los criterios conocidos y explica las
+# conclusiones obtenidas.
+
+
+### Solución II ----------------------------------------------------------------
